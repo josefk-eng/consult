1f8af8e9ceb5a9569ac9ac98a905dc789c171561
josefk-eng <josefkigozi@gmail.com>
2024-04-30 00:47:46 -0700

    Updates

--- assets/js/main.js
+++ assets/js/main.js
@@ -116,10 +116,10 @@
    * Mobile nav toggle
    */
   on('click', '.mobile-nav-toggle', function(e) {
+    console.log("Bar Clicked")
     select('#navbar').classList.toggle('navbar-mobile')
     this.classList.toggle('bi-list')
     this.classList.toggle('bi-x')
-    console.log("Bar Clicked")
   })
 
   /**
